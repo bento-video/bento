17c2dbd689692755a7871afe8d5a7597989d25d7
Mike del Rio <mikevdelrio@gmail.com>
2020-03-09 20:13:21 -0400

    refactored to accept argument as specified in readme, extracted functions from primary function

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 - S3 name and object keys hardcoded
 - lambda not trigerred by events
+
+Assumptions:
+
+- params is an object:
+  {
+  chunkCount = numberOfFilesToConcat
+  fileFormat = extensionOfTranscodedFiles
+  videoName = chosenName
+  }
+
+- transcoded files naming will be `output000.ext, output001.ext...`
+
+- name of the manifest file found in S3 `merge_manifest.txt`
+  - file 'path'(key) on lambda will be `/tmp/${params.videoName}/output000.ext`
--- handler.js
+++ handler.js
@@ -1,88 +1,70 @@
 const { spawnSync } = require("child_process");
 const { readFileSync, writeFileSync, unlinkSync } = require("fs");
 const AWS = require("aws-sdk");
-
 const s3 = new AWS.S3();
 
-module.exports.simpleMerge = async (event, context) => {
-  // if (!event.Records) {
-  //   console.log("not an s3 invocation!");
-  //   return;
-  // }
+const strTemplate = "output000";
+const templateLength = strTemplate.length;
 
-  // for (const record of event.Records) {
-  // r/v purpose of this outer for loop when refactoring to response to events
-
-  // write concat txt file to lambda temp
-  let s3Object = await s3
-    // get the file
-    .getObject({
-      Bucket: "testencodeinput",
-      Key: "concat_list.txt"
-    })
-    .promise();
-  // write file to disk
-  writeFileSync(`/tmp/concat_list.txt`, s3Object.Body);
-
-  // write each chunk to lambda temp,
-  const strTemplate = "output000";
-  const templateLength = strTemplate.length;
-
-  for (let num = 0; num < 41; num += 1) {
-    let keep_char_count = templateLength - String(num).length;
+const loopChunks = (params, action => {
+  for (let num = 0; num < params.chunkCount; num += 1) {
     let suffix = String(num);
+    let keep_char_count = templateLength - suffix.length;
+    let prefix = strTemplate.slice(0, keep_char_count);
 
-    let chunkKey = strTemplate.slice(0, keep_char_count) + suffix + ".mp4";
+    let chunkKey = `${prefix}${suffix}.${params.fileFormat}`;
 
-    //get the file
-    let s3Object = await s3
-      .getObject({
-        Bucket: "testencodeinput",
-        Key: chunkKey
-      })
-      .promise();
-    // write file to disk
-    writeFileSync(`/tmp/` + chunkKey, s3Object.Body);
+    action(params, chunkKey); 
   }
+});
+
+const getFiles = (params, chunkKey => {
+  //get the file
+  let s3Object = await s3
+  .getObject({
+    Bucket: "testencodeinput",
+    Key: chunkKey
+  })
+  .promise();
+  // write file to disk
+  writeFileSync(`/tmp/${params.videoName}/${chunkKey}`, s3Object.Body);
+});
+
+const unlink = (chunkCount) => {unlinkSync(chunkKey)}
+
+module.exports.simpleMerge = async params => {
+  const concatFilePath = `/tmp/${params.videoName}/${params.videoName}.${params.fileFormat}`;
+  const manifestPath = `/tmp/${params.videoName}/merge_manifest.txt`
+
+  // write each chunk to lambda temp
+  loopChunks(params, getFiles);
+
   // merge files stored on lambda
   spawnSync(
     "/opt/ffmpeg/ffmpeg",
-
     [
-      "-f",
-      "concat",
-      "-safe",
-      "0",
-      "-i",
-      `/tmp/concat_list.txt`,
-      "-c",
-      "copy",
-      "/tmp/humility_transcoded.mp4"
+      "-f", "concat",
+      "-safe", "0",
+      "-i", manifestPath,
+      "-c", "copy",
+      concatFilePath
     ],
     { stdio: "inherit" }
   );
-  // read .mov from disk
-  const concatenatedFile = readFileSync("/tmp/humility_transcoded.mp4");
+  // read concatenated file from disk
+  const concatFile = readFileSync(concatFilePath);
 
   // delete the temp files
-  unlinkSync(`/tmp/humility_transcoded.mp4`);
-  unlinkSync(`/tmp/concat_list.txt`);
+  unlinkSync(concatFilePath);
+  unlinkSync(manifestPath);
 
-  for (let num = 0; num < 41; num += 1) {
-    let keep_char_count = templateLength - String(num).length;
-    let suffix = String(num);
-
-    let chunkKey =
-      `/tmp/` + strTemplate.slice(0, keep_char_count) + suffix + ".mp4";
-
-    unlinkSync(chunkKey);
-  }
+  loopChunks(_, unlink);
 
   // upload mp4 to s3
   await s3
     .putObject({
       Bucket: "testencodeoutput",
-      Key: "humility_transcoded_720.mp4",
+      Key: `${params.videoName}.${params.fileFormat}`,
       Body: concatenatedFile
     })
     .promise();
