b7a1059d7ccf9f1186ff28edf6dcc8351b94a3b2
Mike del Rio <mikevdelrio@gmail.com>
2020-03-11 14:56:42 -0400

    handler no longer accepts an argument, previous argument vals are fetched from DB, write to DB to change job status to completed at end of function

--- handler.js
+++ handler.js
@@ -3,41 +3,98 @@ const { readFileSync, writeFileSync, unlinkSync } = require("fs");
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
-const strTemplate = "output000";
-const templateLength = strTemplate.length;
+AWS.config.update({
+  region: "us-east-2",
+  endpoint: "https://dynamodb.us-east-2.amazonaws.com"
+});
 
-const loopChunks = (params, action => {
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const getJobData = async dbQueryParams => {
+  return await docClient.get(dbQueryParams, function (err, data) {
+    if (err) {
+      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("GetItem succeeded:");
+      return data;
+    }
+  }).promise();
+}
+
+// const loopChunks = (params, action => {
+//   for (let num = 0; num < params.chunkCount; num += 1) {
+//     let suffix = String(num);
+//     let keep_char_count = templateLength - suffix.length;
+//     let prefix = chunkNameTemplate.slice(0, keep_char_count);
+
+//     let chunkKey = `${prefix}${suffix}.${params.fileFormat}`;
+
+//     action(params, chunkKey);
+//   }
+// });
+
+// const getFiles = async(params, chunkKey => {
+//   //get the file
+//   let s3Object =
+//     await s3.getObject({
+//       Bucket: "testencodeinput",
+//       Key: chunkKey
+//     })
+//       .promise();
+//   // write file to disk
+//   writeFileSync(`/tmp/${params.videoName}/${chunkKey}`, s3Object.Body);
+// });
+
+// const unlink = (chunkCount) => { unlinkSync(chunkKey) }
+
+module.exports.simpleMerge = async () => {
+  const table = "Jobs";
+  const id = 12345;
+
+  const dbQueryParams = {
+    TableName: table,
+    Key: { id }
+  };
+
+  // is this await still required?
+  const jobData = await getJobData(dbQueryParams);
+
+  const jobId = String(jobData.Item.id);
+  const fileFormat = jobData.Item.outputType;
+  const chunkCount = jobData.Item.totalTasks;
+  const filename = jobData.Item.filename;
+
+  const params = {
+    jobId,
+    fileFormat,
+    chunkCount
+  };
+
+  const chunkNameTemplate = `${jobId}-000`;
+  const templateLength = chunkNameTemplate.length;
+
+  const concatFilePath = `/tmp/${jobId}/${jobId}-${filename}.${fileFormat}`;
+  const manifestPath = `/tmp/${jobId}/merge-manifest.txt`
+
+  // write each chunk to lambda temp
+  // loopChunks(params, getFiles);
+
   for (let num = 0; num < params.chunkCount; num += 1) {
     let suffix = String(num);
     let keep_char_count = templateLength - suffix.length;
-    let prefix = strTemplate.slice(0, keep_char_count);
+    let prefix = chunkNameTemplate.slice(0, keep_char_count);
 
     let chunkKey = `${prefix}${suffix}.${params.fileFormat}`;
 
-    action(params, chunkKey); 
+    console.log(chunkKey);
+
+    let s3Object = await s3.getObject({
+      Bucket: "testencodeinput",
+      Key: chunkKey
+    }).promise();
+    //write file to disk
+    writeFileSync(`/tmp/${params.batchId}/${chunkKey}`, s3Object.Body);
   }
-});
-
-const getFiles = (params, chunkKey => {
-  //get the file
-  let s3Object = await s3
-  .getObject({
-    Bucket: "testencodeinput",
-    Key: chunkKey
-  })
-  .promise();
-  // write file to disk
-  writeFileSync(`/tmp/${params.videoName}/${chunkKey}`, s3Object.Body);
-});
-
-const unlink = (chunkCount) => {unlinkSync(chunkKey)}
-
-module.exports.simpleMerge = async params => {
-  const concatFilePath = `/tmp/${params.videoName}/${params.videoName}.${params.fileFormat}`;
-  const manifestPath = `/tmp/${params.videoName}/merge_manifest.txt`
-
-  // write each chunk to lambda temp
-  loopChunks(params, getFiles);
 
   // merge files stored on lambda
   spawnSync(
@@ -58,14 +115,46 @@ module.exports.simpleMerge = async params => {
   unlinkSync(concatFilePath);
   unlinkSync(manifestPath);
 
-  loopChunks(_, unlink);
+  for (let num = 0; num < params.chunkCount; num += 1) {
+    let suffix = String(num);
+    let keep_char_count = templateLength - suffix.length;
+    let prefix = chunkNameTemplate.slice(0, keep_char_count);
+
+    let chunkKey = `${prefix}${suffix}.${params.fileFormat}`;
+
+    unlinkSync(chunkKey)
+  }
 
   // upload mp4 to s3
   await s3
     .putObject({
       Bucket: "testencodeoutput",
-      Key: `${params.videoName}.${params.fileFormat}`,
-      Body: concatenatedFile
+      Key: `${params.batchId}.${params.fileFormat}`,
+      Body: concatFile
     })
     .promise();
+
+  // update job status on DB to completed
+
+  const putParams = {
+    TableName: table,
+    Key: { 'id': id },
+    UpdateExpression: "set #s = :stat",
+    ExpressionAttributeNames: {
+      "#s": "status"
+    },
+    ExpressionAttributeValues: {
+      ":stat": "completed"
+    },
+    ReturnValues: `UPDATED_NEW`
+  };
+
+  console.log("Updating the item...");
+  docClient.update(putParams, function (err, data) {
+    if (err) {
+      console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+  });
 };
