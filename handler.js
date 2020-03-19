3baaba54980dabbbc46a93100085c6009531be30
Max Hawkins <thinkybeast@gmail.com>
2020-03-19 16:01:42 -0700

    refactor for memory optimization

--- handler.js
+++ handler.js
@@ -2,96 +2,78 @@ const { spawnSync } = require("child_process");
 const { readFileSync, writeFileSync, unlinkSync } = require("fs");
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
-
-AWS.config.update({
-  region: "us-east-1",
-  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
-});
-
 const docClient = new AWS.DynamoDB.DocumentClient();
+const transcodedChunksBucket = process.env.TRANSCODED_CHUNKS_BUCKET;
+const endBucket = process.env.END_BUCKET;
+const jobsTable = "Jobs";
+const manifestPath = `/tmp/manifest.ffcat`;
 
-const getJobData = async dbQueryParams => {
-  console.log('Getting data from jobs table: ', dbQueryParams);
+const getJobData = async jobId => {
+  const dbQueryParams = {
+    TableName: jobsTable,
+    Key: { id: jobId }
+  };
+
+  console.log('Getting data from jobs table: ', JSON.stringify(dbQueryParams));
+
   return await docClient.get(dbQueryParams, function (err, data) {
     if (err) {
       console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-      console.log("GetItem succeeded:", data);
+      console.log("Get from Jobs table succeeded: ", JSON.stringify(data));
       return data;
     }
   }).promise();
 };
 
 const writeJob = async putParams => {
-  console.log("Updating the item...");
+  console.log(`Updating jobs table with params: ${JSON.stringify(putParams)}`);
   return await docClient.update(putParams, function (err, data) {
     if (err) {
       console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+      console.log("Update to table succeeded:", JSON.stringify(data, null, 2));
     }
   }).promise();
 };
 
-module.exports.simpleMerge = async (event) => {
-  const transcodedChunksBucket = process.env.TRANSCODED_CHUNKS_BUCKET;
-  const endBucket = process.env.END_BUCKET;
-
-  const tableName = "Jobs";
-  const jobId = event.jobId;
-
-  const dbQueryParams = {
-    TableName: tableName,
-    Key: { id: jobId }
-  };
-
-  const jobData = await getJobData(dbQueryParams);
-
-  const jobStartTime = jobData.Item.createdAt;
-  const inputFormat = jobData.Item.inputType;
-  const fileFormat = jobData.Item.outputType;
-  const chunkCount = jobData.Item.totalTasks;
-  const filename = jobData.Item.filename;
-
-  const localMasterFilePath = `/tmp/${filename}${fileFormat}`;
-  const localFinalFilePath = `/tmp/${filename}-final${fileFormat}`;
-  const localOriginalFilePath = `/tmp/${filename}-original${inputFormat}`;
-  const localManifestPath = `/tmp/manifest.ffcat`;
-
-  const localChunkFilePrefix = `/tmp/${jobId}-`;
-  let chunkFileSuffix;
-
-  // write each chunk to lambda temp
-
+const getSegments = async ({ id: jobId, outputType, totalTasks }) => {
   console.log('Grabbing segments from transcodedChunksBucket');
-  for (let num = 0; num < chunkCount; num += 1) {
-    let digitCount = String(num).length;
+  for (let num = 0; num < totalTasks; num += 1) {
 
-    if (digitCount === 1) {
-      chunkFileSuffix = `00${String(num)}${fileFormat}`;
-    } else if (digitCount === 2) {
-      chunkFileSuffix = `0${String(num)}${fileFormat}`;
-    } else if (digitCount === 3) {
-      chunkFileSuffix = `${String(num)}${fileFormat}`;
-    }
+    let segmentId = String(num).padStart(3, '0');
+    let segmentKey = `${jobId}/${jobId}-${segmentId}${outputType}`;
+    let segmentPath = `/tmp/${jobId}-${segmentId}${outputType}`
 
-    let s3chunkKey = `${jobId}/${jobId}-${chunkFileSuffix}`;
-    let localChunkFilePath = localChunkFilePrefix + chunkFileSuffix;
 
-    console.log('Getting s3chunkKey ', s3chunkKey, ' from ', transcodedChunksBucket);
-    console.log('localChunkFilePath', localChunkFilePath);
+    console.log('Getting segmentKey ', segmentKey, ' from ', transcodedChunksBucket);
 
     let s3Object = await s3.getObject({
       Bucket: transcodedChunksBucket,
-      Key: s3chunkKey
+      Key: segmentKey
     }).promise();
     //write file to disk
-    console.log('Received chunk body:', s3Object);
+    console.log('Received segment ', segmentId);
 
-    console.log('Attempting write to : ', localChunkFilePath);
-    writeFileSync(localChunkFilePath, s3Object.Body);
+    console.log('Attempting write to : ', segmentPath);
+    writeFileSync(segmentPath, s3Object.Body);
   }
+}
 
+const unlinkSegmentsAndManifest = ({ id: jobId, outputType, totalTasks }) => {
+  for (let num = 0; num < totalTasks; num += 1) {
+
+    let segmentId = String(num).padStart(3, '0');
+    let segmentPath = `/tmp/${jobId}-${segmentId}${outputType}`
+
+    console.log(`Unlinking ${segmentPath}`)
+    unlinkSync(segmentPath);
+  }
+  console.log(`Unlinking ${manifestPath}`)
+  unlinkSync(manifestPath);
+}
+
+const getManifest = async (jobId) => {
   // write manifest
   console.log('Getting manifest from s3');
   let s3Object = await s3.getObject({
@@ -100,95 +82,106 @@ module.exports.simpleMerge = async (event) => {
   }).promise();
 
   //write file to disk
+  console.log(`Writing manifest to ${manifestPath}`)
+  writeFileSync(manifestPath, s3Object.Body);
+}
 
-  writeFileSync(localManifestPath, s3Object.Body);
-  console.log('post get/write manifest and pre merge');
-
-  console.log('getting original video: ', `${filename}${inputFormat}`);
+const getOriginalVideo = async (originalVideoPath, jobData) => {
+  const { filename, inputType, hasAudio } = jobData
+  if (!hasAudio) {
+    console.log(`Video has no audio stream: won't fetch original video.`)
+    return;
+  }
+  console.log(`Retrieving original video from start bucket: ${filename}${inputType}`);
 
   // get original video
-  s3Object = await s3.getObject({
+  let s3Object = await s3.getObject({
     Bucket: 'bento-video-start',
-    Key: `${filename}${inputFormat}`
+    Key: `${filename}${inputType}`
   }).promise();
 
+  console.log(`Writing original video to ${originalVideoPath}`)
   // save original video to /tmp
-  writeFileSync(localOriginalFilePath, s3Object.Body);
+  writeFileSync(originalVideoPath, s3Object.Body);
+}
 
-  // merge files stored on lambda and save to /tmp
+const mergeSegmentsIntoVideo = mergedSegmentsPath => {
+  // merge segments into single file and save to /tmp
+  console.log(`Merging segments into single video, saving to ${mergedSegmentsPath}`)
   spawnSync(
     "/opt/ffmpeg/ffmpeg",
     [
       "-f", "concat",
       "-safe", "0",
-      "-i", localManifestPath,
+      "-i", manifestPath,
       "-c", "copy",
-      localMasterFilePath
+      mergedSegmentsPath
     ],
     { stdio: "inherit" }
   );
+}
 
+const buildCompletedVideo = (videoPaths, jobData) => {
+  if (!jobData.hasAudio) {
+    console.log(`Video has no audio stream: completed path is now ${videoPaths.mergedPath}`);
+    videoPaths.completedPath = videoPaths.mergedPath;
+    return;
+  }
+  const { mergedPath, originalPath, completedPath } = videoPaths
   spawnSync(
     "/opt/ffmpeg/ffmpeg",
     [
-      "-i", localMasterFilePath,
-      "-i", localOriginalFilePath,
+      "-i", mergedPath,
+      "-i", originalPath,
       "-c", "copy",
       "-map", "0:v",
       "-map", "1:a",
-      localFinalFilePath
+      completedPath
     ]
   );
+}
 
-  // read concatenated file from disk
-  const finalFile = readFileSync(localFinalFilePath);
+const unlinkMergedVideo = (mergedPath) => {
+  console.log(`Unlinking ${mergedPath}`)
+  unlinkSync(mergedPath);
+}
 
-  console.log('masterFile has been read');
-  // delete the tmp files
-  unlinkSync(localMasterFilePath);
-  unlinkSync(localManifestPath);
-  unlinkSync(localOriginalFilePath);
-  unlinkSync(localFinalFilePath);
-
-  for (let num = 0; num < chunkCount; num += 1) {
-    let digitCount = String(num).length;
-
-    if (digitCount === 1) {
-      chunkFileSuffix = `00${String(num)}${fileFormat}`;
-    } else if (digitCount === 2) {
-      chunkFileSuffix = `0${String(num)}${fileFormat}`;
-    } else if (digitCount === 3) {
-      chunkFileSuffix = `${String(num)}${fileFormat}`;
-    }
-
-    let localChunkFilePath = localChunkFilePrefix + chunkFileSuffix;
-
-    console.log('unlinking');
-    console.log('localChunkFilePath', localChunkFilePath);
-
-    unlinkSync(localChunkFilePath);
+const unlinkOriginalVideo = (originalVideoPath, jobData) => {
+  if (!jobData.hasAudio) {
+    return;
   }
+  console.log(`Unlinking  ${originalVideoPath}`)
+  unlinkSync(originalVideoPath);
+}
 
-  // upload mp4 to s3
+const putCompletedVideoInBucket = async (jobData, completedVideoPath) => {
+  const { id: jobId, filename, outputType } = jobData;
+  console.log(`Reading completed file from ${completedVideoPath}`)
+  const completedVideo = readFileSync(completedVideoPath);
+
+  // delete the tmp files
+  console.log('Completed file read into memory, unlinking file at path ', completedVideoPath);
+  unlinkSync(completedVideoPath);
+
+  const completedKey = `${jobId}-${filename}${outputType}`;
+  console.log(`Putting completed video ${completedKey} in bucket ${endBucket}`);
   await s3
     .putObject({
       Bucket: endBucket,
-      Key: `${jobId}-${filename}${fileFormat}`,
-
-      Body: finalFile
+      Key: completedKey,
+      Body: completedVideo
     })
     .promise();
+}
 
+const recordJobCompleted = async ({ id: jobId, createdAt }) => {
   // update job status on DB to completed
 
   const jobEndTime = Date.now();
-  const timeToCompleteSeconds = (jobEndTime - jobStartTime) / 1000;
-  // const timeToCompleteMinutes = Math.floor(timeToCompleteSeconds / 60);
-  // const remainingSeconds = Math.floor(timeToCompleteSeconds % 60);
-  // const jobTimeToComplete = `${timeToCompleteMinutes}:${remainingSeconds}`;
+  const timeToCompleteSeconds = (jobEndTime - createdAt) / 1000;
 
   const putParams = {
-    TableName: tableName,
+    TableName: jobsTable,
     Key: { 'id': jobId },
     UpdateExpression: "set #s = :stat, completedAt = :completedAt, timeToComplete = :timeToComplete",
     ExpressionAttributeNames: {
@@ -203,4 +196,33 @@ module.exports.simpleMerge = async (event) => {
   };
 
   await writeJob(putParams);
+}
+
+module.exports.simpleMerge = async (event) => {
+  const jobId = event.jobId;
+  const jobData = await getJobData(jobId);
+  jobData = { ...jobData.Item };
+
+  const { inputType, outputType, filename } = jobData;
+
+  const videoPaths = {
+    mergedPath: `/tmp/${filename}-Merged${outputType}`,
+    originalPath: `/tmp/${filename}-Original${inputType}`,
+    completedPath: `/tmp/${filename}-Completed${outputType}`
+  }
+
+  await getSegments(jobData);
+  await getManifest(jobId);
+
+  mergeSegmentsIntoVideo(videoPaths.mergedPath);
+  unlinkSegmentsAndManifest(jobData)
+
+  await getOriginalVideo(videoPaths.originalPath, jobData)
+
+  buildCompletedVideo(videoPaths, jobData);
+  unlinkMergedVideo(videoPaths.mergedPath);
+  unlinkOriginalVideo(videoPaths.originalPath, jobData)
+
+  await putCompletedVideoInBucket(jobData, videoPaths.completedPath)
+  await recordJobCompleted(jobData);
 };
