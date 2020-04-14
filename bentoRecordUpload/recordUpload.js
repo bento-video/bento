16431455f149e409e8ef7b50d159db4e8c1b9ed6
Nathan Classen <hayesclassen@gmail.com>
2020-04-14 11:36:48 -0500

    added new Lambda to deployment; records video uploads to DynamoDB:BentoVideos

---
+++ bentoRecordUpload/recordUpload.js
@@ -0,0 +1,59 @@
+'use strict';
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+const DDB = new AWS.DynamoDB.DocumentClient();
+const bucket = process.env.NEW_VIDEO_BUCKET;          // notify Mike of this ENV variable. Should be set up already.
+const { readFileSync, unlinkSync } = require("fs");
+const { execSync } = require("child_process");
+const { extname } = require('path');
+
+const probeVideoDataPath = "/tmp/probeVideoData.json";
+const videosTable = process.env.VIDEOS_TABLE;         // notify Mike of this ENV var. Should be set up already. Needs to be BentoVideos rather than Videos
+
+const probeVideo = objectUrl => {
+  const command = `/opt/ffmpeg/ffprobe -v error -show_entries stream=width,height -show_entries format=size,duration -print_format json -i "${objectUrl}" > "${probeVideoDataPath}"`;
+
+  execSync(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`ERROR when probing for video data: ${error}`);
+      return;
+    }
+  });
+
+  const probeData = readFileSync(probeVideoDataPath);
+  return JSON.parse(probeData);
+};
+
+module.exports.recordUpload = async (event) => {
+  const id = event.id;
+  const filename = event.filename;
+  const format = extname(filename);
+
+  global.gc();
+
+  const urlParams = { Bucket: bucket, Key: filename, Expires: 900 };
+  const signedUrl = s3.getSignedUrl('getObject', urlParams);
+  const probeData = probeVideo(signedUrl);
+
+  const { width, height } = probeData.streams[0];
+  const { duration, size } = probeData.format;
+  const resolution = `${width}x${height}`;
+
+  const videoDbParams = {
+    TableName: videosTable,
+    Item: { id, filename, format, resolution, duration, size, versions: 0 }
+  };
+
+  const dbEntry = await DDB.put(videoDbParams).promise()
+    .then(data => {
+      console.log("Added video");
+      return videoDbParams;
+    })
+    .catch(err => {
+      console.log(`Unable to add video. Error JSON:`, JSON.stringify(err, null, 2));
+    });
+
+  console.log("DB ENTRY: ", dbEntry);
+  unlinkSync(probeVideoDataPath);
+  return dbEntry.Item;
+};
