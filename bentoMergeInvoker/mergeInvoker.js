6df810944e552049bde237ac3e7f3eb42c468d67
Max Hawkins <1227297+thinkybeast@users.noreply.github.com>
2020-04-16 12:26:13 -0700

    integrate with updated table schema

--- bentoMergeInvoker/mergeInvoker.js
+++ bentoMergeInvoker/mergeInvoker.js
@@ -1,26 +1,32 @@
 const AWS = require("aws-sdk");
 const lambda = new AWS.Lambda({
-  region: "us-east-1"
+  region: "us-east-1",
 });
-const mergeFunction = process.env.MERGER_LAMBDA_ADDRESS;
+const mergeFunction = process.env.mergeLamdaArn;
 
 exports.mergeInvoke = async (event) => {
   const eventRecord = event.Records[0];
   const dbEventType = eventRecord.eventName;
+  console.log("MSSG:'mergeInvoke'(func): [FIRED] event: ", event);
 
   if (dbEventType !== "MODIFY") {
-    console.log(`\nFrom 'mergeInvoke'(func):[NOT A MODIFY] -DBEntryType: ${dbEventType}\nnot a modification of an entry in the Jobs table.`);
+    console.log(
+      `\nMSSG:'mergeInvoke'(func): [NOT A MODIFY] -DBEntryType: ${dbEventType}\nnot a modification of an entry in the Jobs table.`
+    );
     return;
   }
 
   const entryInfo = eventRecord.dynamodb.NewImage;
+  console.log(entryInfo);
 
   if (entryInfo.status.S !== "pending") {
-    console.log(`\nFrom 'mergeInvoke'(func):[NO INVOCATION REQUEST] \njobs table was updated, not in such a way so as to invoke the merge function.`);
+    console.log(
+      `\nMSSG:'mergeInvoke'(func): [NO INVOCATION REQUEST] \njobs table was updated, not in such a way so as to invoke the merge function.`
+    );
     return;
   }
 
-  const jobID = entryInfo.id.S;
+  const jobID = Number(entryInfo.id.N); //jobId is Number in updated table
   const finishedTasks = entryInfo.finishedTasks.N;
   const totalTasks = entryInfo.totalTasks.N;
 
@@ -28,21 +34,25 @@ exports.mergeInvoke = async (event) => {
     FunctionName: mergeFunction,
     InvocationType: "Event",
     Payload: JSON.stringify({ jobId: jobID }),
-    LogType: 'None',
-    ClientContext: 'MergeInvokerFunction',
+    LogType: "None",
+    ClientContext: "MergeInvokerFunction",
   };
 
   if (finishedTasks === totalTasks) {
-    console.log(`\nFrom 'mergeInvoke'(func):[EQUAL TASKS] \n'merge'(func) should be invoked on this condition`);
+    console.log(
+      `\nMSSG:'mergeInvoke'(func): [EQUAL TASKS] \n'merge'(func) should be invoked on this condition`
+    );
 
-    console.log('Attempting merge invoke...')
+    console.log("MSSG: Attempting merge invoke...");
 
-    await lambda.invoke(mergeParams).promise()
-      .then(data => {
+    await lambda
+      .invoke(mergeParams)
+      .promise()
+      .then((data) => {
         console.log("MSSG: Merge invoked successfully");
         console.log("Payload: ", data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("MSSG: Encountered error when trying to invoke merge");
         console.log(error);
       });
